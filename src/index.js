9f413e08e4b99c5d0c038986b5a5a6809b8f785b
NOVACHRONO <benjamincath@gmail.com>
2020-01-23 23:13:23 +0100

    wahala

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const {PORT = 3000} = process.env
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(cors())
 
 app.use("/api/v1", require("./routes"))
 
--- src/models/db.js
+++ src/models/db.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require("mongodb")
-
+require("dotenv").config()
 const {MONGO_URI, DB_NAME} = process.env
 
 const client = new MongoClient(MONGO_URI, {
