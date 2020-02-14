6a8b7c30eb02657f47acf74354fd55e7ae16b865
enigmaIam <nwagba001@gmail.com>
2020-02-14 06:35:49 +0100

    Fix/ Variable not defined
    - Fixed DOC.md, added route for delisting admin

--- src/controllers/business/vendors.js
+++ src/controllers/business/vendors.js
@@ -27,7 +27,7 @@ module.exports.destroy = async request => {
     const vendor = { businesses =[], id: vendorId } = await Account.find({ email: vendorEmail })
     if (!vendor) throw new ResponseError(400, "You can't remove a non-existent vendor account")
     const isAVendorOf = businesses && businesses.includes(id)
-    if (!isAVendorOf) throw new ReponseError(400, "Vendor is not a part of your business")
+    if (!isAVendorOf) throw new ResponseError(400, "Vendor is not a part of your business")
     const index = businesses.indexOf(id)
     delete businesses[index]
     await Account.update({ _id: vendorId }, { businesses })
