0f8b77c2ea52e85ba94f7bc8123849683e059995
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2020-03-06 02:51:50 +0100

    Feature/ Added service_location and service_category fields for vendor accounts

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -2,25 +2,17 @@ const Collection = require("../../data/orm")
 const bcrypt = require("bcryptjs")
 const { ResponseError, Response } = require("../../utils")
 const tokeniser = require("../../services/tokeniser")
+const V = require("../../services/validator")
 
 const Account = Collection("accounts")
 
 module.exports = async request => {
     const { name, email, userType, password, phone } = request.body
 
-    if (!email || !name || !userType || !password || !phone) {
-        throw new ResponseError(400, "You need to provide name, email, userType, password, phone")
-    }
-    if (!(/^(business|regular|vendor)$/.test(userType))) {
-        throw new ResponseError(400, "userType must be one of business, regular, vendor")
-    }
-    if (!(/\+234\ ?\d{3}\ ?\d{3}\ ?\d{4}/.test(phone))) {
-        throw new ResponseError(400, "phone must be of the form +234 --- --- ----")
-    }
-    if (!(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email))) {
-        throw new ResponseError(400, "email is not valid")
-    }
-
+    V.allExist("You need to provide name, email, userType, password, phone", email, name, userType, password, phone)
+        .matchesRegex("userType must be one of business, regular, vendor", userType, /^(business|regular|vendor)$/)
+        .matchesRegex("phone must be of the form +234 --- --- ----", phone, /^\+234\ ?\d{3}\ ?\d{3}\ ?\d{4}$/)
+        .isEmail("email is not valid", email);
     const hashedPassword = bcrypt.hashSync(password)
 
     const emailExists = await Account.find({ email }).then(user => !!user)
@@ -28,14 +20,30 @@ module.exports = async request => {
     const phoneExists = await Account.find({ phone }).then(user => !!user)
     if (phoneExists) throw new ResponseError(400, "phone is already in use")
 
-    const data = await Account.insert({
-        email,
-        name,
-        userType,
-        password: hashedPassword,
-        phone,
-        isVerified: false,
-    })
+    let data = {}
+    if (userType === 'vendor') {
+        const { oem, service_category, service_location } = request.body
+        V.allExist("You must provide oem, service_category and service_location for a vendor account before registering", oem, service_category, service_location)
+        data = await Account.insert({
+            email,
+            name,
+            userType,
+            password: hashedPassword,
+            phone,
+            isVerified: false,
+            oem, service_category, service_location
+        })
+    } else {
+        data = await Account.insert({
+            email,
+            name,
+            userType,
+            password: hashedPassword,
+            phone,
+            isVerified: false,
+        })
+    }
+
     const token = tokeniser.create({
         email: data.email,
         phone: data.phone,
--- src/services/validator.js
+++ src/services/validator.js
@@ -8,7 +8,8 @@ class V {
      */
     static isEmail(message, data) {
         const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-        V.matchesRegex(data, regex, message)
+        V.matchesRegex(message, data, regex)
+        return V
     }
     /**
      * 
@@ -20,6 +21,7 @@ class V {
         if(!regex.test(data)){
             throw new ResponseError(400, message)
         }
+        return V
     }
     /**
      * 
@@ -31,6 +33,7 @@ class V {
         if(fields.some(x => x === null || x === undefined)){
             throw new ResponseError(400, message)
         }
+        return V
     }
     /**
      * 
@@ -41,6 +44,7 @@ class V {
         if(!expression){
             throw new ResponseError(400, message)
         }
+        return V
     }
 }
 
