0f42eb79d165b56aac0f7ba2066ddff4891cda67
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2020-03-17 10:37:49 +0100

    Fix: dateJoined issue after page has been made

--- src/routes/admin-invite.js
+++ src/routes/admin-invite.js
@@ -1,14 +1,19 @@
-require("dotenv").config()
-const { CLIENT_APP } = process.env
-const c = require("../data/collections")
+require("dotenv").config();
+const { CLIENT_APP } = process.env;
+const c = require("../data/collections");
 
 module.exports = async (request, response) => {
-    const { token: invite_token } = request.query
+  const { token: invite_token } = request.query;
 
-    const modified = await c.business_admin_rel.update({ invite_token }, { accepted: true })
-    const bar = await c.business_admin_rel.find({invite_token})
-    const user = await c.accounts.find({_id: bar.userId})
-    return modified
-        ? response.redirect(`${CLIENT_APP}/register?type=${user.userType}&email=${user.email}`)
-        : response.redirect(CLIENT_APP)
-}
+  const modified = await c.business_admin_rel.update(
+    { invite_token },
+    { accepted: true, dateJoined: Date.now() }
+  );
+  const bar = await c.business_admin_rel.find({ invite_token });
+  const user = await c.accounts.find({ _id: bar.userId });
+  return modified
+    ? response.redirect(
+        `${CLIENT_APP}/register?type=${user.userType}&email=${user.email}`
+      )
+    : response.redirect(CLIENT_APP);
+};
