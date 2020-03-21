eb499ec5c1ace48c9e3477f5cfff2510a19f1646
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2020-03-21 18:57:24 +0100

    Fix: removed duplicate products

--- src/controllers/search/product.js
+++ src/controllers/search/product.js
@@ -5,17 +5,6 @@ module.exports = async request => {
   const { q: searchQuery, by = "name" } = request.query;
   if (!searchQuery) throw new ResponseError(400, "You must send a query");
 
-  const products = await c.inventory
-    .findAll({ [by]: new RegExp(searchQuery, "i") })
-    .then(x => {
-      return x.map(({ id, name, price, description, oem }) => ({
-        id,
-        name,
-        description,
-        price,
-        oem
-      }));
-    });
   const products = await c.inventory.aggregate([
     {
       $match: { [by]: new RegExp(searchQuery, "i") }
