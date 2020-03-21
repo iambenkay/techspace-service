b81a640bf362a7bba4bbd86175e811076e8f88ce
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2020-03-21 18:39:57 +0100

    Feat: Improved product search to include vendor info

--- src/controllers/search/product.js
+++ src/controllers/search/product.js
@@ -1,16 +1,50 @@
-const c = require("../../data/collections")
-const { Response, ResponseError } = require("../../utils")
+const c = require("../../data/collections");
+const { Response, ResponseError } = require("../../utils");
 
 module.exports = async request => {
-    const { q: searchQuery, by = "name" } = request.query
-    if (!searchQuery) throw new ResponseError(400, "You must send a query")
+  const { q: searchQuery, by = "name" } = request.query;
+  if (!searchQuery) throw new ResponseError(400, "You must send a query");
 
-    const products = await c.inventory.findAll({ [by]: new RegExp(searchQuery, "i") }).then(x => {
-        return x.map(({ id, name, description, oem }) => ({ id, name, description, price, oem }))
-    })
-
-    return new Response(200, {
-        error: false,
-        products
-    })
-}
+  const products = await c.inventory
+    .findAll({ [by]: new RegExp(searchQuery, "i") })
+    .then(x => {
+      return x.map(({ id, name, price, description, oem }) => ({
+        id,
+        name,
+        description,
+        price,
+        oem
+      }));
+    });
+  const products = await c.inventory.aggregate([
+    {
+      $match: { [by]: new RegExp(searchQuery, "i") }
+    },
+    {
+      $lookup: {
+        from: "accounts",
+        localField: "vendorId",
+        foreignField: "_id",
+        as: "vendor"
+      }
+    },
+    {
+      $unwind: "$vendor"
+    },
+    {
+      $project: {
+        name: true,
+        price: true,
+        description: true,
+        oem: true,
+        "vendor.name": true,
+        "vendor._id": true,
+        "vendor.service_category": true
+      }
+    }
+  ]);
+  return new Response(200, {
+    error: false,
+    products
+  });
+};
