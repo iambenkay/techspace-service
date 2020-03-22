af59b9c209fdc0e9cb0ce5d7ccdeb3572a1adaf7
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2020-03-22 02:53:33 +0100

    Feature: business can query vendor inventory

--- src/controllers/vendors/inventory.js
+++ src/controllers/vendors/inventory.js
@@ -1,56 +1,67 @@
-const c = require("../../data/collections")
-const { Response, ResponseError } = require("../../utils")
-const V = require("../../services/validator")
-
+const c = require("../../data/collections");
+const { Response, ResponseError } = require("../../utils");
+const V = require("../../services/validator");
 
 module.exports.add = async request => {
-    const { id } = request.payload
-    const { name, description, price, oem } = request.body
+  const { id } = request.payload;
+  const { name, description, price, oem } = request.body;
 
-    V.allExist("You must provide name, description, price and oem", name, description, price, oem)
+  V.allExist(
+    "You must provide name, description, price and oem",
+    name,
+    description,
+    price,
+    oem
+  );
 
-    const data = await c.inventory.insert({
-        name,
-        description,
-        price,
-        oem,
-        vendorId: id,
-    })
-    delete data
-    return new Response(200, {
-        error: false,
-        message: "Product has been succesfully added to Inventory",
-        data
-    })
-}
+  const data = await c.inventory.insert({
+    name,
+    description,
+    price,
+    oem,
+    vendorId: id
+  });
+  delete data;
+  return new Response(200, {
+    error: false,
+    message: "Product has been succesfully added to Inventory",
+    data
+  });
+};
 
 module.exports.remove = async request => {
-    const { id } = request.payload
-    const { productId } = request.body
-    const product = await c.inventory.find({ _id: productId })
-    if (!product) throw new ResponseError(404, "There is no product that matches your query")
-    if (product.vendorId !== id) throw new ResponseError(401, "You are not authorized to delete this product")
-    await c.inventory.remove({ _id: productId })
-    return new Response(200, {
-        error: false,
-        message: "Product has been succesfully removed from Inventory",
-    })
-}
+  const { id } = request.payload;
+  const { productId } = request.body;
+  const product = await c.inventory.find({ _id: productId });
+  if (!product)
+    throw new ResponseError(404, "There is no product that matches your query");
+  if (product.vendorId !== id)
+    throw new ResponseError(
+      401,
+      "You are not authorized to delete this product"
+    );
+  await c.inventory.remove({ _id: productId });
+  return new Response(200, {
+    error: false,
+    message: "Product has been succesfully removed from Inventory"
+  });
+};
 
 module.exports.retrieveAll = async request => {
-    const { id } = request.payload
+  let { id, userType } = request.payload;
+  if (userType === "business") id = request.body.id;
+  if (!id) throw new ResponseError(400, "There is no vendor with that ID");
+  const data = await c.inventory.findAll({ vendorId: id });
 
-    const data = await c.inventory.findAll({ vendorId: id })
-
-    return new Response(200, {
-        error: false,
-        data
-    })
-}
+  return new Response(200, {
+    error: false,
+    data
+  });
+};
 
 module.exports.retrieve = async request => {
-    const { id } = request.payload
+  const { id } = request.payload;
 
-    const { id: productId } = request.body
-    const data = await c.inventory.find({ _id: productId }) 
-}
+  const { id: productId } = request.body;
+  const data = await c.inventory.find({ _id: productId });
+};
