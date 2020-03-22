70f2d1badbbaf029a43eedd7d604679dc2ed981c
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2020-03-22 18:13:37 +0100

    Fix: Fixed pending businesses GET

--- src/controllers/vendors/fulfill-requirements.js
+++ src/controllers/vendors/fulfill-requirements.js
@@ -6,7 +6,6 @@ module.exports = async request => {
   const { id: vId } = request.payload;
 
   const { businessId, type, id, value } = request.body;
-  console.log(request.body)
   request.V.allExist(
     "You must provide: businessId, type and id",
     businessId,
