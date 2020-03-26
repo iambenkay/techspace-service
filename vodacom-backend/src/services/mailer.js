b69df43c8317cc957eebe7e012de9176d2b67166
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2020-03-26 21:16:52 +0100

    Format: Prettier v2

--- vodacom-backend/src/services/mailer.js
+++ vodacom-backend/src/services/mailer.js
@@ -1,40 +1,49 @@
-const nodemailer = require("nodemailer")
-const { EMAIL_HOST, EMAIL_USER, EMAIL_PASSWORD, EMAIL_PORT, EMAIL_USE_TLS } = process.env
+const nodemailer = require("nodemailer");
+const {
+  EMAIL_HOST,
+  EMAIL_USER,
+  EMAIL_PASSWORD,
+  EMAIL_PORT,
+  EMAIL_USE_TLS
+} = process.env;
 
 class Mail {
-    /**
-     * 
-     * @param {String} from 
-     * @param {String[]} to 
-     * @param {String} subject 
-     * @param {String} text 
-     * @param {String} html 
-     */
-    constructor(to, subject, text, html) {
-        this.to = to.join(", ")
-        this.subject = subject
-        this.text = text
-        this.html = html
-    }
-    send() {
-        try {
-
-            let transporter = nodemailer.createTransport({
-                host: EMAIL_HOST,
-                port: EMAIL_PORT,
-                secure: EMAIL_USE_TLS == 0 ? false: true,
-                auth: {
-                    user: EMAIL_USER,
-                    pass: EMAIL_PASSWORD
-                }
-            })
-            transporter.sendMail({
-                from: '"Vendor Alliance" <support@vodacomgroup.com>', to: this.to, subject: this.subject, text: this.text, html: this.html
-            })
-        } catch (e) {
-            console.error(e.message)
+  /**
+   *
+   * @param {String} from
+   * @param {String[]} to
+   * @param {String} subject
+   * @param {String} text
+   * @param {String} html
+   */
+  constructor(to, subject, text, html) {
+    this.to = to.join(", ");
+    this.subject = subject;
+    this.text = text;
+    this.html = html;
+  }
+  send() {
+    try {
+      let transporter = nodemailer.createTransport({
+        host: EMAIL_HOST,
+        port: EMAIL_PORT,
+        secure: EMAIL_USE_TLS == 0 ? false : true,
+        auth: {
+          user: EMAIL_USER,
+          pass: EMAIL_PASSWORD
         }
+      });
+      transporter.sendMail({
+        from: '"Vendor Alliance" <support@vodacomgroup.com>',
+        to: this.to,
+        subject: this.subject,
+        text: this.text,
+        html: this.html
+      });
+    } catch (e) {
+      console.error(e.message);
     }
+  }
 }
 
-module.exports = Mail
+module.exports = Mail;
