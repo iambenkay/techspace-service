08f1e1fe4d4e02d74d8d8214c550a446dbff822c
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2020-03-27 08:48:08 +0100

    FIx: ...

--- vodacom-backend/src/controllers/rfqs/index.js
+++ vodacom-backend/src/controllers/rfqs/index.js
@@ -109,6 +109,7 @@ module.exports.explore = async request => {
   if (type === "private") q.vendor = vendor.id;
   if (type === "private-group") q.business_category = vendor.business_category;
   if (type === "public") q.service_category = vendor.service_category;
+  console.log(q);
   const data = await c.rfq.aggregate([
     {
       $match: q
