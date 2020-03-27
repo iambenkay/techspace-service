6ed4361a70fbb4969381cfc0642edf03f91ef4d5
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2020-03-27 01:12:59 +0100

    Fix: Worker, proto workdir not found

--- vodacom-wq/src/rpcs/mailer.js
+++ vodacom-wq/src/rpcs/mailer.js
@@ -4,7 +4,7 @@ const logger = require("../services/logger");
 
 const Mail = require("../services/mailer");
 
-const mail_package_def = proto_loader.loadSync("src/protos/mail.proto");
+const mail_package_def = proto_loader.loadSync("vodacom-wq/src/protos/mail.proto");
 
 const mail_package = grpc.loadPackageDefinition(mail_package_def).MailService;
 
