baadd1821c9f80ae903c84299536fd195acbda20
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2020-03-27 01:17:12 +0100

    Fix: ...

--- vodacom-wq/src/index.js
+++ vodacom-wq/src/index.js
@@ -8,4 +8,5 @@ const server = new grpc.Server();
 server.addService(MailService.package.service, MailService.impl);
 
 server.bind(VODACOM_WORKER, grpc.ServerCredentials.createInsecure());
+console.log(`gRPC Server running at ${VODACOM_WORKER}`);
 server.start();
