d0657e313128756bd466152cf2e8469f1b875dd1
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2020-04-03 18:26:53 +0100

    Fix: 500 error

--- vodacom-backend/src/routes/archives.js
+++ vodacom-backend/src/routes/archives.js
@@ -1,11 +1,33 @@
 const router = require("express").Router();
 const handler = require("../services/request-injector");
 const archive = require("../controllers/archives");
+const { isAuthenticated, isAccountType } = require("../middleware");
 const upload = require("multer")();
 
-router.post("/archives", upload.single("document"), handler(archive.add));
-router.delete("/archives", handler(archive.remove));
-router.get("/archives", handler(archive.fetchAll));
-router.get("/archives/:arch_id", handler(archive.fetch));
+router.post(
+  "/archives",
+  isAuthenticated,
+  isAccountType("business", "vendor"),
+  upload.single("document"),
+  handler(archive.add)
+);
+router.delete(
+  "/archives",
+  isAuthenticated,
+  isAccountType("business", "vendor"),
+  handler(archive.remove)
+);
+router.get(
+  "/archives",
+  isAuthenticated,
+  isAccountType("business", "vendor"),
+  handler(archive.fetchAll)
+);
+router.get(
+  "/archives/:arch_id",
+  isAuthenticated,
+  isAccountType("business", "vendor"),
+  handler(archive.fetch)
+);
 
 module.exports = router;
