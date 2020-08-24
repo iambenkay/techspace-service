2eee2b8f56f160ac784750e436702c00f47f2452
Benjamin Chibuzor-Orie <benjamincath@gmail.com>
2020-08-24 19:46:34 +0100

    fixes rejected status for quotes

--- vodacom-backend/src/controllers/business/rejectVendor.js
+++ vodacom-backend/src/controllers/business/rejectVendor.js
@@ -1,24 +1,22 @@
 const c = require("../../data/collections");
-const {Response, ResponseError} = require("../../utils");
+const { Response, ResponseError } = require("../../utils");
 
-module.exports = async request => {
-  const {id} = request.payload;
-  const {vendorId, review, comment} = request.body;
-  if (review === true && !comment) throw new ResponseError(400, "You must send a comment for review");
+module.exports = async (request) => {
+  const { id } = request.payload;
+  const { vendorId, review, comment } = request.body;
+  if (review === true && !comment)
+    throw new ResponseError(400, "You must send a comment for review");
   request.V.allExist("You must provide vendorId", vendorId);
-  const update = {accepted: false, dateJoined: Date.now()};
+  const update = { accepted: false, dateJoined: Date.now() };
   if (review === true) {
     update.rejectComment = comment;
-  } else {
-    update.rejected = true;
   }
-  await c.business_vendor_rel.update(
-    {businessId: id, vendorId},
-    update
-  );
+  update.rejected = true;
+  await c.business_vendor_rel.update({ businessId: id, vendorId }, update);
 
   return new Response(200, {
     error: false,
-    message: "Application was " +  review === true ? "returned for review" : "rejected"
+    message:
+      "Application was " + review === true ? "returned for review" : "rejected",
   });
 };
